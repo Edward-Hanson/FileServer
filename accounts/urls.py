9ca070ec63097291bb05200aa3748b4524102e85
Edward Hanson <ehanson787@gmail.com>
2023-06-25 20:51:48 +0000

    structuring signup and login views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, activate_account
+from .views import SignUpView, activate_account, login_View
 
 urlpatterns = [
     path('signup/',SignUpView,name='signup'),
     path('activate/<str:uidb64>/<str:token>/', activate_account, name='activate_account'),
+    path('login/',login_View,name='login'),
 ]
