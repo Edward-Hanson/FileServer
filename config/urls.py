203b8776abf853a04efed38955f25662c5f73fa5
Edward Hanson <ehanson787@gmail.com>
2023-06-26 08:38:43 +0000

    configuration of the top level urls

--- config/urls.py
+++ config/urls.py
@@ -24,9 +24,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
-    path('blog/',include('blog.urls')),
+    path('',include('blog.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
-    path('',TemplateView.as_view(template_name= 'home.html'),name= 'home'),
 ]
 
 if settings.DEBUG:
